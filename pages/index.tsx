f258c58c047a8bcfe207c50bbe35de69b08619f4
Syahrul Anuar <syahrlanuar95@gmail.com>
2022-11-21 01:05:00 +0800

    Add prettier codebase styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import Head from 'next/head'
+import Head from "next/head";
 
 export default function Home() {
     return (
@@ -6,25 +6,46 @@ export default function Home() {
             <Head>
                 <title>SYAHRULBUILDS</title>
                 <link rel="shorcut icon" href="images/favicon.ico" />
-                <link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon.png" />
-                <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png" />
-                <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16.png" />
+                <link
+                    rel="apple-touch-icon"
+                    sizes="180x180"
+                    href="/images/apple-touch-icon.png"
+                />
+                <link
+                    rel="icon"
+                    type="image/png"
+                    sizes="32x32"
+                    href="/images/favicon-32x32.png"
+                />
+                <link
+                    rel="icon"
+                    type="image/png"
+                    sizes="16x16"
+                    href="/images/favicon-16x16.png"
+                />
             </Head>
-            <div className='h-screen flex flex-col items-center justify-center'>
+            <div className="flex h-screen flex-col items-center justify-center">
                 <div>
-                    <h1 className='font-bold text-3xl tracking-tighter text-black text-transparent bg-clip-text bg-[radial-gradient(ellipse_at_bottom_right,_var(--tw-gradient-stops))] from-zinc-400 via-fuchsia-800 to-rose-200 pb-2
-                    text-center lg:text-left'>
+                    <h1
+                        className="bg-[radial-gradient(ellipse_at_bottom_right,_var(--tw-gradient-stops))] from-zinc-400 via-fuchsia-800 to-rose-200 bg-clip-text pb-2 text-center text-3xl font-bold tracking-tighter text-black
+                    text-transparent lg:text-left"
+                    >
                         Syahrulbuilds.space is currently under further upgrades.
                     </h1>
-                    <h2 className='text-2xl font-bold tracking-tighter pb-8 underline text-center lg:text-left'>
+                    <h2 className="pb-8 text-center text-2xl font-bold tracking-tighter underline lg:text-left">
                         Design + Code.
                     </h2>
-                    <p className='text-center lg:text-left'>
-                        Syahrulbuilds.space is <a href="https://github.com/syahshiimi" className='font-semibold text-fuchsia-500/90 hover:underline underline-offset-4'>Syahrul Anuar</a>
+                    <p className="text-center lg:text-left">
+                        Syahrulbuilds.space is{" "}
+                        <a
+                            href="https://github.com/syahshiimi"
+                            className="font-semibold text-fuchsia-500/90 underline-offset-4 hover:underline"
+                        >
+                            Syahrul Anuar
+                        </a>
                     </p>
                 </div>
             </div>
         </>
-
-    )
+    );
 }
